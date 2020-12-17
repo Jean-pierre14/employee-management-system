53288f603be0d8404f414e48f37012b178f3495f
Grace El Bisimwa <58594917+Jean-pierre14@users.noreply.github.com>
2020-12-17 22:23:42 +0200

    Message design navbar

--- msg.php
+++ msg.php
@@ -1,15 +1,17 @@
 <?php require_once 'process/dbh.php'; ?>
 <!DOCTYPE html>
-<html lang="en"><!-- Basic -->
+<html lang="en">
+<!-- Basic -->
+
 <head>
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
-   
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
- 
-     <!-- Site Metas -->
-    <title>Tea Factory Management System</title>  
+
+    <!-- Site Metas -->
+    <title>Tea Factory Management System</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -19,9 +21,9 @@
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">    
-	<!-- Site CSS -->
-    <link rel="stylesheet" href="css/style.css">    
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="css/responsive.css">
     <!-- Custom CSS -->
@@ -36,98 +38,88 @@
 
 <body>
 
-	<!-- Start header -->
-	<header class="top-navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="index.html">
-          <img src="images/logonew.png" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbars-rs-food">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item"><a class="nav-link" href="aloginwel.php">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="addemp.php">Add Employee</a></li>
-            
-            <li class="nav-item"><a class="nav-link" href="assign.php">Assign Project</a></li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">All pages</a>
-              <div class="dropdown-menu" aria-labelledby="dropdown-a">
-                <a class="dropdown-item" href="viewemp.php">View Employee</a>
-                <a class="dropdown-item" href="salaryemp.php">Salary Table</a>
-                <a class="dropdown-item" href="empleave.php">Employee Leave</a>
-                <a class="dropdown-item" href="assignproject.php">Project Status</a>
-                <a class="dropdown-item active" href="msg.php">Messages</a>
-                
-              </div>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="sniper.php?logout">LogOut</a></li>
-          </ul>
+    <!-- Start header -->
+    <header class="top-navbar">
+        <nav class="navbar navbar-expand-lg p-0 m-0 navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    <img src="images/logonew.png" alt="" width="200">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food"
+                    aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbars-rs-food">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="aloginwel.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="addemp.php">Add Employee</a></li>
+
+                        <li class="nav-item"><a class="nav-link" href="assign.php">Assign Project</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">All
+                                pages</a>
+                            <div class="dropdown-menu" aria-labelledby="dropdown-a">
+                                <a class="dropdown-item" href="viewemp.php">View Employee</a>
+                                <a class="dropdown-item" href="salaryemp.php">Salary Table</a>
+                                <a class="dropdown-item" href="empleave.php">Employee Leave</a>
+                                <a class="dropdown-item" href="assignproject.php">Project Status</a>
+                                <a class="dropdown-item active" href="msg.php">Messages</a>
+
+                            </div>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" href="sniper.php?logout">LogOut</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!--arelt messages! -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="heading-title text-center" style="margin-top: 80px;">
+                    <h1>Messages</h1>
+                </div>
+            </div>
         </div>
-      </div>
-    </nav>
-  </header>
-	<!--arelt messages! -->
-	
-<div class="container">
-    <div class="row">
-
-    	<div class="col-lg-12">
-        	<div class="heading-title text-center" style="margin-top: 80px;">
-                        
-            	<h1>Messages</h1>
-        	</div>
-    	</div>
-
-    	<div class="col-lg-12">
-        	<div class="heading-title text-center">
-                        
-            	<h1>Messages</h1>
-        	</div>
-    </div>
-</div>  
-
-
-<?php
-
+        <?php
 $result = $conn->query("SELECT * FROM message") or die($mysqli->error);
 //pre_r($result);
 ?>
-<div class="row-justify-content-center">
-	<table class="table table-hover" border="0">
-		<thead>
+        <div class="row justify-content-center">
+            <table class="table table-hover table-sm" border="0">
+                <thead>
 
-			<tr>
-				
-				<th>Name</th>
-				<th>Email</th>
-				<th>Message</th>
-				
-				<th colspan="2">Action</th>
-			</tr>
-		</thead>
-		<?php while($row = $result->fetch_assoc()): ?>
+                    <tr>
 
-				<tr>
-					
-					<td><?php echo $row["name"]; ?></td>
-					<td><?php echo $row["email"]; ?></td>
-					<td><?php echo $row["message"]; ?></td>
-					
-					
-					<td>
-						
-						<a href="process/delete.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
-					</td>
-				</tr>
-				<?php endwhile ?>	
-	</table>
-	
-</div>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Message</th>
 
-<?php
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+                <?php while($row = $result->fetch_assoc()): ?>
+
+                <tr>
+
+                    <td><?php echo $row["name"]; ?></td>
+                    <td><?php echo $row["email"]; ?></td>
+                    <td><?php echo $row["message"]; ?></td>
+
+
+                    <td>
+
+                        <a href="process/delete.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+                <?php endwhile ?>
+            </table>
+
+        </div>
+
+        <?php
 
 pre_r($result->fetch_assoc());
 
@@ -137,43 +129,45 @@ function pre_r( $array ){
 	echo '</pre>';
 }
 ?>
-<!-- contact info -->
+        <!-- contact info -->
 
 
-	<!-- End Contact info -->
-	
-	<!-- Start Footer -->
-	<footer class="footer-area">
-		
-		
-		<div class="copyright">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<p class="company-name">All Rights Reserved. &copy; 2020 <a href="#">Tea Factory Management System</a> Design By : 
-					<a href="https://html.design/">Eloi & Aggee</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-	</footer>
-	<!-- End Footer -->
-	
-	<a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o" aria-hidden="true"></i></a>
+        <!-- End Contact info -->
 
-	<!-- ALL JS FILES -->
-	<script src="js/jquery-3.2.1.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-    <!-- ALL PLUGINS -->
-	<script src="js/jquery.superslides.min.js"></script>
-	<script src="js/images-loded.min.js"></script>
-	<script src="js/isotope.min.js"></script>
-	<script src="js/baguetteBox.min.js"></script>
-	<script src="js/form-validator.min.js"></script>
-    <script src="js/contact-form-script.js"></script>
-    <script src="js/custom.js"></script>
-</div>
+        <!-- Start Footer -->
+        <footer class="footer-area" style="margin-top: 150px;">
+            <div class="copyright">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p class="company-name">All Rights Reserved. &copy; 2020 <a href="#">Tea Factory Management
+                                    System</a> Design By :
+                                <a href="https://html.design/">Eloi & Aggee</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+        <!-- End Footer -->
+
+        <a href="#" id="back-to-top" title="Back to top" style="display: none;"><i class="fa fa-paper-plane-o"
+                aria-hidden="true"></i></a>
+
+        <!-- ALL JS FILES -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- ALL PLUGINS -->
+        <script src="js/jquery.superslides.min.js"></script>
+        <script src="js/images-loded.min.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/baguetteBox.min.js"></script>
+        <script src="js/form-validator.min.js"></script>
+        <script src="js/contact-form-script.js"></script>
+        <script src="js/custom.js"></script>
+    </div>
 </body>
+
 </html>
